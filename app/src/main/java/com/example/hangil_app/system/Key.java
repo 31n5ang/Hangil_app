6bda9743d8681f4063483eec924c210bff3bf9b5
31n5ang <31n5ang@gmail.com>
2024-10-07 20:19:39 +0900

    Update .gitignore

--- app/src/main/java/com/example/hangil_app/system/Key.java
+++
@@ -1,6 +0,0 @@
-package com.example.hangil_app.system;
-
-public class Key {
-    public final static String TMAP_APP_KEY = "";
-    public static final String API_URL = "http://123.456.789.012:8081/api/";
-}
