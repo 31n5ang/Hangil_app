8c1fad552e22d0826d6145beb0324803a04080e9
Song Minsang <31n5ang@gmail.com>
2024-10-07 20:11:25 +0900

    Update Key.java

--- app/src/main/java/com/example/hangil_app/system/Key.java
+++ app/src/main/java/com/example/hangil_app/system/Key.java
@@ -1,6 +1,6 @@
 package com.example.hangil_app.system;
 
 public class Key {
-    public final static String TMAP_APP_KEY = "Ndc3c4Kz0i7Cu5TBb93hF35hivbzqERV5N7XhOiy";
-    public static final String API_URL = "http://13.209.153.63:8081/api/";
+    public final static String TMAP_APP_KEY = "";
+    public static final String API_URL = "http://123.456.789.012:8081/api/";
 }
